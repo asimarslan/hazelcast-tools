c89eb684ea698d3fd26fd89c94fd09c1bd926a6c
Asım Arslan <arslanasim@gmail.com>
2016-03-27 23:02:23 +0300

    plugin project init

---
+++ src/com/hazelcast/idea/plugins/toolcast/UtilityGeneration.java
@@ -0,0 +1,114 @@
+package com.hazelcast.idea.plugins.toolcast;
+
+import java.util.List;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.LanguageLevelModuleExtensionImpl;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.pom.java.LanguageLevel;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementFactory;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.codeStyle.JavaCodeStyleManager;
+import com.intellij.psi.util.PsiTreeUtil;
+
+public abstract class UtilityGeneration extends AnAction {
+    public static final String COM_GOOGLE_COMMON_BASE_OBJECTS = "com.google.common.base.Objects";
+    public static final String JAVA_UTIL_OBJECTS = "java.util.Objects";
+    public static final LanguageLevel JAVA_UTIL_OBJECTS_VERSION = LanguageLevel.JDK_1_7;
+    protected boolean hasJavaUtilObjects = false;
+    private String dialogTitle;
+
+    public UtilityGeneration(String text, String dialogTitle) {
+        super(text);
+        this.dialogTitle = dialogTitle;
+    }
+
+    public void actionPerformed(AnActionEvent e) {
+        hasJavaUtilObjects = findLanguageLevel(e).compareTo(JAVA_UTIL_OBJECTS_VERSION) >= 0;
+
+        PsiClass psiClass = getPsiClassFromContext(e);
+        GenerateDialog dlg = new GenerateDialog(psiClass, dialogTitle);
+        dlg.show();
+        if (dlg.isOK()) {
+            generate(psiClass, dlg.getFields());
+        }
+    }
+
+    private LanguageLevel findLanguageLevel(AnActionEvent e) {
+        Module module = DataKeys.MODULE.getData(e.getDataContext());
+        LanguageLevel languageLevel = LanguageLevel.JDK_1_6;
+
+        Project project = e.getProject();
+        if (project != null) {
+            languageLevel = LanguageLevelProjectExtension.getInstance(project).getLanguageLevel();
+        }
+
+        if (module != null) {
+            LanguageLevel moduleLanguageLevel = LanguageLevelModuleExtensionImpl.getInstance(module).getLanguageLevel();
+            languageLevel = moduleLanguageLevel != null ? moduleLanguageLevel : languageLevel;
+        }
+
+        return languageLevel;
+    }
+
+    abstract public void generate(final PsiClass psiClass, final List<PsiField> fields);
+
+    protected void setNewMethod(PsiClass psiClass, String newMethodBody, String methodName) {
+        PsiElementFactory elementFactory = JavaPsiFacade.getElementFactory(psiClass.getProject());
+        PsiMethod newEqualsMethod = elementFactory.createMethodFromText(newMethodBody, psiClass);
+        PsiElement method = addOrReplaceMethod(psiClass, newEqualsMethod, methodName);
+        JavaCodeStyleManager.getInstance(psiClass.getProject()).shortenClassReferences(method);
+    }
+
+    protected PsiElement addOrReplaceMethod(PsiClass psiClass, PsiMethod newEqualsMethod, String methodName) {
+        PsiMethod existingEqualsMethod = findMethod(psiClass, methodName);
+
+        PsiElement method;
+        if (existingEqualsMethod != null) {
+            method = existingEqualsMethod.replace(newEqualsMethod);
+        }
+        else {
+            method = psiClass.add(newEqualsMethod);
+        }
+        return method;
+    }
+
+    protected PsiMethod findMethod(PsiClass psiClass, String methodName) {
+        PsiMethod[] allMethods = psiClass.getAllMethods();
+        for (PsiMethod method : allMethods) {
+            if (psiClass.getName().equals(method.getContainingClass().getName()) && methodName.equals(method.getName())) {
+                return method;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromContext(e);
+        e.getPresentation().setEnabled(psiClass != null);
+    }
+
+    private PsiClass getPsiClassFromContext(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        return PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+    }
+}
